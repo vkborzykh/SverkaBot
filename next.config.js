6ad61d80a192afe67ed6f6f9bbdf051f1e900319
vkborzykh <150504605+vkborzykh@users.noreply.github.com>
2026-06-15 14:24:34 +0700

    Update next.config.js
    Remove output export to enable API routes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',  // required for Netlify serverless functions
+const nextConfig = {  
   eslint: {
     ignoreDuringBuilds: true,
   },
